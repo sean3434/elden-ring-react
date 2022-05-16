c6a0ad74ab1721cee33db94d4ae2845e3a0afae5
sean3434 <seannyc34@gmail.com>
2022-05-16 11:52:35 -0400

    API Call Succesful For Shields & Weapons Pages

--- src/pages/Gear/Shields.js
+++ src/pages/Gear/Shields.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ShieldsList() {
-  return (
-    <div>Shields</div>
-  )
+    const [shields, setShields] = useState(null)
+
+    function getShields() {
+        fetch('https://eldenring.fanapis.com/api/shields?limit=100&page=0')
+        .then((res) => res.json())
+        .then((res) => setShields(res.data))
+    }
+
+    useEffect(() => {
+        getShields();
+    }, [])
+
+// console.log(shields);
+
+    return (
+        <>
+            {shields ? shields.map((shield, index) => (
+                <div key={index}>
+                    <h2>{shield.name}</h2>
+                    <img src={shield.image} alt={shield.name}/>
+                    {/* <h2>{shield.description}</h2> */}
+                    <hr/>
+            </div>
+            )) : <h3>LOADING...</h3>}
+        </>
+    )
 }
 
 export default ShieldsList
--- src/pages/Gear/Weapons.js
+++ src/pages/Gear/Weapons.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function WeaponsList() {
-  return (
-    <div>Weapons</div>
-  )
+    const [weapons, setWeapons] = useState(null)
+
+    function getWeapons() {
+        fetch('https://eldenring.fanapis.com/api/weapons?limit=100&page=0')
+        .then((res) => res.json())
+        .then((res) => setWeapons(res.data))
+    }
+
+    useEffect(() => {
+        getWeapons();
+    }, [])
+
+console.log(weapons);
+
+    return (
+        <>
+            {weapons ? weapons.map((weapon, index) => (
+                <div key={index}>
+                    <h2>{weapon.name}</h2>
+                    <img src={weapon.image} alt={weapon.name}/>
+                    <h2>{weapon.description}</h2>
+                    <hr/>
+            </div>
+            )) : <h3>LOADING...</h3>}
+        </>
+    )
 }
 
 export default WeaponsList
