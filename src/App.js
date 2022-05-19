6abbc47fd81c8ef34c3d055a1f511ce32f44d26d
sean3434 <seannyc34@gmail.com>
2022-05-19 15:19:57 -0400

    Footer With Working LinkedIn & Github Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Nav"
+import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Magic from "./pages/Magic";
 import Gear from "./pages/Gear";
@@ -22,31 +23,32 @@ import NPCsList from "./pages/World/NPCs";
 
 
 function App() {
-  return(
-    <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Magic" element={<Magic />} />
-        <Route path="Magic/AshesOfWar" element={<AshesOfWarList />}></Route>
-        <Route path="Magic/Incantations" element={<IncantationsList />}></Route>
-        <Route path="Magic/Sorceries" element={<SorceriesList />}></Route>
-        <Route path="Magic/Spirits" element={<SpiritsList />}></Route>
-      <Route path="/Gear" element={<Gear />} />
-        <Route path="Gear/Ammos" element={<AmmosList />}></Route>
-        <Route path="Gear/Armors" element={<ArmorsList />}></Route>
-        <Route path="Gear/Shields" element={<ShieldsList />}></Route>
-        <Route path="Gear/Weapons" element={<WeaponsList />}></Route>
-      <Route path="/Items" element={<ItemsList />} />
-      <Route path="/Talismans" element={<TalismansList />} />
-      <Route path="/World" element={<World />} />
-        <Route path="World/Bosses" element={<BossesList />}></Route>
-        <Route path="World/Creatures" element={<CreaturesList />}></Route>
-        <Route path="World/Locations" element={<LocationsList />}></Route>
-        <Route path="World/NPCs" element={<NPCsList />}></Route>
-    </Routes>
-    </>
-  )
+    return(
+        <>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Magic" element={<Magic />} />
+                <Route path="Magic/AshesOfWar" element={<AshesOfWarList />}></Route>
+                <Route path="Magic/Incantations" element={<IncantationsList />}></Route>
+                <Route path="Magic/Sorceries" element={<SorceriesList />}></Route>
+                <Route path="Magic/Spirits" element={<SpiritsList />}></Route>
+            <Route path="/Gear" element={<Gear />} />
+                <Route path="Gear/Ammos" element={<AmmosList />}></Route>
+                <Route path="Gear/Armors" element={<ArmorsList />}></Route>
+                <Route path="Gear/Shields" element={<ShieldsList />}></Route>
+                <Route path="Gear/Weapons" element={<WeaponsList />}></Route>
+            <Route path="/Items" element={<ItemsList />} />
+            <Route path="/Talismans" element={<TalismansList />} />
+            <Route path="/World" element={<World />} />
+                <Route path="World/Bosses" element={<BossesList />}></Route>
+                <Route path="World/Creatures" element={<CreaturesList />}></Route>
+                <Route path="World/Locations" element={<LocationsList />}></Route>
+                <Route path="World/NPCs" element={<NPCsList />}></Route>
+        </Routes>
+        <Footer />
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaBeer } from 'react-icons/fa';
+import './Footer.css'
+import image from '../Media/zlinkedinlogo.png'
+import image2 from '../Media/zgithub.png'
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<a href='https://www.linkedin.com/in/sean-ortiz1/'> <img className='linkedin' src={image} alt='LinkedIn'/></a>
+			<a href='https://github.com/sean3434'> <img className='github' src={image2} alt='Github'/></a>
+            <div className='footerbottomtext'> Copyright © 2022 Sean Ortiz</div>
+		</div>
+	)
+}
+
+export default Footer
