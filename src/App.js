578bdfc47847ff3abfe4f0e0cbbababbc0a267a7
sean3434 <seannyc34@gmail.com>
2022-05-19 19:16:06 -0400

    Netlify Error Fix Part 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/nav.js"
-import Footer from "./components/footer.js"
+import Footer from "./components/Footer.js"
 import Home from "./pages/home.js"
 import Magic from "./pages/magic.js";
 import Gear from "./pages/gear.js";
